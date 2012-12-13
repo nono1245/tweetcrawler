d9c531ec2509dffb67901a95b4e5b06e97785c78
nono1245 <candysynd@gmail.com>
2012-12-14 03:25:02 +0900

    Update stream_all.rb

--- stream_all.rb
+++ stream_all.rb
@@ -5,10 +5,10 @@ require 'rubygems'
 require 'tweetstream' 
 
 TweetStream.configure do |config|
-  config.consumer_key = 'KTtr7SE6CX0nI4yFg4s3fg'
-  config.consumer_secret = '3aBc0kp3wYyQY1tPfjkVMx7tpIJmVbqL5m5belghM'
-  config.oauth_token = '458569192-u8sBOurXqUqDdwexz0pt24hqwhw47JDTxckQLU'
-  config.oauth_token_secret = 'mRO2xXnhkwuY8B7vnKXlLDVJnriYB57eVPHKZRilwk'
+  config.consumer_key = 'hogehoge'
+  config.consumer_secret = 'hogehoge'
+  config.oauth_token = 'hogehoge'
+  config.oauth_token_secret = 'hogehoge'
   config.auth_method = :oauth
   config.parser   = :yajl	#using Yajl-Ruby
 end
