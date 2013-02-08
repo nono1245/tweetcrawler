afcb5700c826e66ebbeaab5f4cf0616c8e224406
nono1245 <candysynd@gmail.com>
2013-02-08 18:27:05 +0900

    Update stream_ja.rb

--- stream_ja.rb
+++ stream_ja.rb
@@ -3,19 +3,27 @@
 
 require 'rubygems'
 require 'tweetstream' 
+require 'yaml' #txtに書いたkeyなどを呼び出せる
+require 'eventmachine' #非同期処理で高速動作
 
+CONF = YAML::load(open("config.yaml")) #API keyを呼び出す
+ 
 TweetStream.configure do |config|
-  config.consumer_key = 'KTtr7SE6CX0nI4yFg4s3fg'
-  config.consumer_secret = '3aBc0kp3wYyQY1tPfjkVMx7tpIJmVbqL5m5belghM'
-  config.oauth_token = '458569192-u8sBOurXqUqDdwexz0pt24hqwhw47JDTxckQLU'
-  config.oauth_token_secret = 'mRO2xXnhkwuY8B7vnKXlLDVJnriYB57eVPHKZRilwk'
-  config.auth_method = :oauth
+  config.consumer_key       = CONF["consumer_key"]
+  config.consumer_secret    = CONF["consumer_secret"]
+  config.oauth_token        = CONF["access_token"]
+  config.oauth_token_secret = CONF["access_token_secret"]
+  config.auth_method        = :oauth
   config.parser   = :yajl  #using Yajl-Ruby
 end
  
+
+
+#TweetStream::Client.new.track('hoge') do |status|
+
 TweetStream::Client.new.sample do |status|
     if status.user.lang == 'ja'
-        puts "#{status.text}"
-        #puts "#{status.user.screen_name} : #{status.text}"
+	reply = status.in_reply_to_user_id == nil ? "P" : "R"
+        puts "#{status.created_at} : #{reply} : #{status.user.screen_name} : #{status.text}"
     end
 end
